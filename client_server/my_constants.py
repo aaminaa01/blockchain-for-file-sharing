dc08948c77caa4e5ff158019c706e73573ba514d
Navaira Rehman <navairarehman@Navairas-MacBook-Air.local>
2024-12-20 15:28:14 +0500

    feat: increase buffer size and user upload limit

--- client_server/my_constants.py
+++ client_server/my_constants.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-DOWNLOAD_FOLDER = r"\Users\PMYLS\Desktop\7th semester\IS\Project\blockchain-for-file-sharing\client_server\downloads"
-TEMP_FOLDER = r"\Users\PMYLS\Desktop\7th semester\IS\Project\blockchain-for-file-sharing\client_server\temp_folder"
+DOWNLOAD_FOLDER = r'/Users/navairarehman/Documents/Semester/Information Security /blockchain-for-file-sharing/client_server/downloads'
+TEMP_FOLDER = r"/Users/navairarehman/Documents/Semester/Information Security /blockchain-for-file-sharing/client_server/temp_folder"
 
 app = Flask(__name__)
 app.secret_key = "secret key"
@@ -10,7 +10,7 @@ app.config['TEMP_FOLDER'] = TEMP_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app.config['SERVER_IP'] = '127.0.0.1:5111'
-app.config['NODE_ADDR'] = {'Host' : '127.0.0.2', 'Port' : 5113}
-# app.config['NODE_ADDR'] = {'Host' : '0.0.0.0', 'Port' : 5113}
-app.config['BUFFER_SIZE'] = 64 * 1024
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+# app.config['NODE_ADDR'] = {'Host' : '127.0.0.2', 'Port' : 5113}
+app.config['NODE_ADDR'] = {'Host' : '0.0.0.0', 'Port' : 5113}
+app.config['BUFFER_SIZE'] = 256 * 1024 #changed the buffer size from 64 to 256kb/512kb
+app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024 #allow user to upload file upto 32mb
--- main_server/my_constants.py
+++ main_server/my_constants.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
-# UPLOAD_FOLDER = '/Users/souviksaha/Desktop/Blockchain-based-Decentralized-File-Sharing-System-using-IPFS/main_server/uploads'
-DOWNLOAD_FOLDER = r"\Users\PMYLS\Desktop\7th semester\IS\Project\blockchain-for-file-sharing\main_server\downloads"
-TEMP_FOLDER = r"\Users\PMYLS\Desktop\7th semester\IS\Project\blockchain-for-file-sharing\main_server\temp_folder"
+DOWNLOAD_FOLDER = r"/Users/navairarehman/Documents/Semester/Information Security /blockchain-for-file-sharing/client_server/downloads"
+TEMP_FOLDER = r"/Users/navairarehman/Documents/Semester/Information Security /blockchain-for-file-sharing/client_server/temp_folder"
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['TEMP_FOLDER'] = TEMP_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-app.config['BUFFER_SIZE'] = 64 * 1024
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['BUFFER_SIZE'] = 256 * 1024 #changed the buffer size from 64 to 256kb/512kb
+app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024 #allow user to upload file upto 32mb
